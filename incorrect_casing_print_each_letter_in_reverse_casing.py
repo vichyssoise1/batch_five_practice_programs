e38ca6cd6516ef25658d3a15d8d92c8389749b51
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-28 22:07:14 +0800

    added .swapcase() to reverse each casing

--- incorrect_casing_print_each_letter_in_reverse_casing.py
+++ incorrect_casing_print_each_letter_in_reverse_casing.py
@@ -1,3 +1,4 @@
 #Basic user input
 full_name = input("Enter your full name in incorrect casing: ")
+full_name = full_name.swapcase()  # Swap the case of each letter
 print("you entered: ", full_name)
