3187f4eafc720c9e929e6ad4e9cfaa3c6455fdc4
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-28 22:17:46 +0800

    added len to count the number of characters

--- number_of_characters_in_name.py
+++ number_of_characters_in_name.py
@@ -1,3 +1,4 @@
 #user input
 full_name = input("Enter your full name: ")
-print("Your full name is: ", full_name)
+full_name = len(full_name) # count the number of characters in the name
+print("Character Count: ", full_name)
