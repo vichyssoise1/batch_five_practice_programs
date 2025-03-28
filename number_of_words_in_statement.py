029808403c29feefe8f82888ed8bc6d8a0220dff
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-28 22:17:01 +0800

    changed the print promt to better match the output

--- number_of_words_in_statement.py
+++ number_of_words_in_statement.py
@@ -1,4 +1,4 @@
 #User input
 statement = input("Enter a statement: ")
 statement = len(statement.split()) # Count the number of words in the statement
-print("You entered: ", statement)
+print("Number of words: ", statement)
