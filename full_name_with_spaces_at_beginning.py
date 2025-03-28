a417b939a0d6e4d241e49fe6c3fca49a88a7c26c
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-28 21:34:45 +0800

    added string manipulation .lstrip() to remove leading spaces

--- full_name_with_spaces_at_beginning.py
+++ full_name_with_spaces_at_beginning.py
@@ -1,3 +1,4 @@
 #Add basic user input functionality to the code
 full_name = input("Enter your full name with spaces at the beginning: ")
-print("Your full name is:", full_name) 
+full_name = full_name.lstrip()  # Remove leading and trailing spaces
+print("Your full name without leading spaces is:", full_name) 
