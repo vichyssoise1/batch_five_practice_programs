356d6a32081b43868d48fa1edd1105295d98188d
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-28 22:04:01 +0800

    added .title() to print in proper casing

--- incorect_casing_print_in_proper_casing.py
+++ incorect_casing_print_in_proper_casing.py
@@ -1,3 +1,4 @@
 #basic user input
-full_name = input("Enter your full name: ")
-print("You entered: ", full_name)
+full_name = input("Enter your full name in improper casing: ")
+full_name = full_name.title()  # Convert to title case (first letter of each word capitalized)
+print("Your name in proper casing is: ", full_name)
