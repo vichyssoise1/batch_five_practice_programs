92dfe1405d55e739cd70a92b91af67c2a08fbaeb
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-28 22:29:07 +0800

    added .lower().replace() to print in snake case

--- full_name_snake_case.py
+++ full_name_snake_case.py
@@ -1,3 +1,4 @@
 #User input
 full_name = input("Enter your full name in incorrect casing: ")
-print("Your name in snake case is:", full_name )
+snake_case = full_name.lower().replace(" ", "_")  # Convert to snake case
+print("Your name in snake case is:", snake_case)
