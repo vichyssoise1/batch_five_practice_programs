136c10cc3a8bc287ca181661273f30003de37a87
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-28 21:55:49 +0800

    added .upper() to print letters in all capital letters

--- full_name_all_capital.py
+++ full_name_all_capital.py
@@ -1,3 +1,4 @@
 #Basic user input to collect full name
-full__name = input("Please enter your full name: ")
-print("Your full name is: ", full__name)
+full_name = input("Please enter your full name: ")
+full_name = full_name.upper() # convert to all capital letters
+print("Your full name in all capital letters is: ", full_name)
