0f21029ba3a15762d731e605d296c99b52f8fec4
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-28 22:01:08 +0800

    added .lower() to make all letters lowercase

--- full_name_all_lower.py
+++ full_name_all_lower.py
@@ -1,3 +1,4 @@
 #basic user input
 full_name = input("Enter your full name: ")
+full_name = full_name.lower()  # convert to all lowercase letters
 print("Your full name in all lowercase is: ", full_name)
